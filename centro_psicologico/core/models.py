788bb6179e8e5470afe5cf2081a4a7d9af5b263d
Ruben14f <mansilla8090@gmail.com>
2024-06-21 15:14:47 -0400

    dd

--- centro_psicologico/core/models.py
+++ centro_psicologico/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 #arreglo de opciones para tipo_consulta
--- centro_psicologico/core/urls.py
+++ centro_psicologico/core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('quienes-somos/', QuienesSomosView.as_view(), name="quienes-somos"),
     path('carrito/', CarritoView.as_view(), name="carrito"),
     path('pagos/', PagosView.as_view(), name="pagos"),
-    path('listar-productos/', listar_reserva, name="listar_reserva"),
-    path('modificar-productos/<id>/', modificar_reserva, name="modificar_reserva"),
-    path('eliminar-productos/<id>/', eliminar_reserva, name="eliminar_reserva"),
+    path('listar-reservas/', listar_reserva, name="listar_reserva"),
+    path('modificar-reservas/<id>/', modificar_reserva, name="modificar_reserva"),
+    path('eliminar-reservas/<id>/', eliminar_reserva, name="eliminar_reserva"),
 ]
