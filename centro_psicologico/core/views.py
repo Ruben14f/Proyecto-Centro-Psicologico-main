7532c14c54fa2e37cd21d6a4f077a2296f545906
Ruben14f <mansilla8090@gmail.com>
2024-06-30 19:36:36 -0400

    table

--- centro_psicologico/core/views.py
+++ centro_psicologico/core/views.py
@@ -5,6 +5,8 @@ from .models import ReservaHora
 from django.shortcuts import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.core.paginator import Paginator
+from django.http import Http404
 
 # Create your views here.
 
@@ -40,8 +42,17 @@ class PagosView(TemplateView):
 def listar_reserva(request):
     
     reservas = ReservaHora.objects.all()
+    page = request.GET.get('page',1)
+    
+    try:
+        paginator = Paginator(reservas, 5)
+        reservas = paginator.get_page(page)
+    except:
+        raise Http404
+    
     data = {
-        'reservas': reservas
+        'reservas': reservas,
+        'paginator': paginator
     }
     return render(request, 'core/reserva/listar.html', data)
 def modificar_reserva(request, id):
